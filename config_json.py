0e79293dba6d6062048f1852ee3d48126b3a6373
pranish-38 <khawaspranish3@gmail.com>
2026-02-19 12:17:30 +0545

    json

---
+++ config_json.py
@@ -0,0 +1,34 @@
+{
+    "monitored_paths": [
+        "/etc/passwd",
+        "/etc/shadow",
+        "/etc/sudoers",
+        "/root/.ssh",
+        "/home"
+    ],
+    "suspicious_processes": [
+        "nc",
+        "ncat",
+        "netcat",
+        "reverse_shell",
+        "mimikatz",
+        "john",
+        "hydra",
+        "nmap",
+        "metasploit"
+    ],
+    "blocked_ports": [
+        4444,
+        5555,
+        6666,
+        31337,
+        12345
+    ],
+    "whitelist_ips": [
+        "127.0.0.1",
+        "::1"
+    ],
+    "max_cpu_percent": 90,
+    "max_memory_percent": 80,
+    "alert_mode": "log"
+}
